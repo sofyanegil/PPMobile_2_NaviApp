56dad87d8801c89b489a54d689edf21cef84437d
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-05-14 10:18:52 +0700

    Call Main in MainActivity

--- app/src/main/java/id/ac/unpas/naviapp/MainActivity.kt
+++ app/src/main/java/id/ac/unpas/naviapp/MainActivity.kt
@@ -3,12 +3,7 @@ package id.ac.unpas.naviapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import id.ac.unpas.naviapp.ui.theme.NaviAppTheme
 
@@ -18,26 +13,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             NaviAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Main()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NaviAppTheme {
-        Greeting("Android")
+        Main()
     }
 }
